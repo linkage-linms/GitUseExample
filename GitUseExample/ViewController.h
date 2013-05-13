0723db940a27ba48e5c444c66590884fc535cff8
Linkage-linms <kenvinny@163.com>
2013-05-13 10:48:26 +0800

    Initial Commit

---
+++ GitUseExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitUseExample
+//
+//  Created by ken on 13-5-13.
+//  Copyright (c) 2013年 ken. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GitUseExample/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GitUseExample
+//
+//  Created by ken on 13-5-13.
+//  Copyright (c) 2013年 ken. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
