775a611d6aba009b9044fbb29c055b08fbcc4cf8
Linkage-linms <kenvinny@163.com>
2013-05-13 11:13:08 +0800

    Map

---
+++ GitUseExample/MapForIternerary.h
@@ -0,0 +1,13 @@
+//
+//  MapForIternerary.h
+//  GitUseExample
+//
+//  Created by ken on 13-5-13.
+//  Copyright (c) 2013年 ken. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapForIternerary : NSObject
+
+@end
---
+++ GitUseExample/MapForIternerary.m
@@ -0,0 +1,13 @@
+//
+//  MapForIternerary.m
+//  GitUseExample
+//
+//  Created by ken on 13-5-13.
+//  Copyright (c) 2013年 ken. All rights reserved.
+//
+
+#import "MapForIternerary.h"
+
+@implementation MapForIternerary
+
+@end
